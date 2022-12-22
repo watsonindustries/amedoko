7a68eb6b07b60bf0975122377fae171471a84d08
Daniils Petrovs <thedanpetrov@gmail.com>
2022-12-22 02:11:59 +0100

    Move image into separate component

---
+++ src/lib/DokoImage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let src = "nagi-amedoko.jpeg";
+    let imageName = "Ame Doko gator by Nabi";
+</script>
+
+<a
+    href="https://twitter.com/Nagi_Nyaaa/status/1598539547184230400"
+    class="mx-auto my-3 max-w-lg drop-shadow-lg"
+>
+    <img {src} alt={imageName} />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
+    import DokoImage from "$lib/DokoImage.svelte";
     import type { PageData } from "./$types";
-
-    let src = "nagi-amedoko.jpeg";
-    let imageName = "Ame Doko gator by Nabi";
 
     export let data: PageData;
     console.log(data);
@@ -10,12 +8,13 @@
 
 <div class="flex flex-col text-ame-dark-brown my-6">
     <h1 class="text-5xl font-bold text-center">Ame Doko?</h1>
-    <span class="text-center">Last stream was <p class="font-bold">{data.lastStreamDays} ago</p></span>
-    <span class="text-center">Next stream in <p class="font-bold">{data.nextStreamDays}</p></span>
-    <a
-        href="https://twitter.com/Nagi_Nyaaa/status/1598539547184230400"
-        class="mx-auto my-3 max-w-lg drop-shadow-lg"
+    <span class="text-center"
+        >Last stream was <p class="font-bold">
+            {data.lastStreamDays} ago
+        </p></span
     >
-        <img {src} alt={imageName} />
-    </a>
+    <span class="text-center"
+        >Next stream in <p class="font-bold">{data.nextStreamDays}</p></span
+    >
+    <DokoImage />
 </div>
