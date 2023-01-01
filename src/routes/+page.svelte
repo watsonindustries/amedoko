237a91ddfe3564cb61c1b57fe1f22393e0d3ac54
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-01 04:40:40 +0100

    Add live now stream button Closes Show current stream when Ame is live #5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,36 @@
     import type { PageData } from "./$types";
     import {deltaFormatted} from '../utils';
 
+    import {Icon, Play} from 'svelte-hero-icons';
+
     export let data: PageData;
     let currentDate = new Date();
     console.log(data);
 
-    let {pastVideo, nextVideo} = data;
+    let {pastVideo, nextVideo, liveVideo} = data;
     let lastStreamDelta = deltaFormatted(currentDate, pastVideo.actualStart || pastVideo.scheduledStart || pastVideo.publishedAt);
 </script>
 
-<div class="flex flex-col text-ame-dark-brown my-6">
+<div class="flex flex-col text-ame-dark-brown text-center my-6">
     <h1 class="text-5xl font-bold text-center my-2">Ame Doko?</h1>
     
+    {#if !liveVideo}
     <span class="text-center"
         >Last stream was <a href="https://youtu.be/{pastVideo.videoId}"><p class="font-bold">
             {lastStreamDelta} ago
-        </p></a></span
-    >
+        </p></a></span>
+    {/if}
+
+    {#if liveVideo}
+    <div class="mx-auto my-3">
+        <h2 class="text-3xl">Live NOW 🎥</h2>
+        <span class="text-center"> 
+            <a href="https://youtu.be/{liveVideo.videoId}" type="button" class="flex flex-row text-ame-light-yellow bg-ame-dark-brown hover:bg-stone-600 focus:ring-4 focus:ring-stone-300 font-medium rounded-lg text-sm px-5 py-2.5 my-3 focus:outline-none">
+                <Icon src="{Play}" solid size="20" class="mr-1" />Watch on YouTube
+            </a>
+        </span>    
+    </div>
+    {/if}
 
     {#if nextVideo}
     <span class="text-center"
