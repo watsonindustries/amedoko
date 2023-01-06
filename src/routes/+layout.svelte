6a187c1e1ffe97036fa99d89f2e4893354db22c0
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-06 18:43:10 +0100

    Change twitter meta type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
   twitter={{
     handle: '@danirukun',
     site: '@amedoko',
-    cardType: 'summary',
+    cardType: 'app',
     title: 'Ame Doko?',
     description: 'Find out where is Ame!',
     image: amedokoLogo,
