42f1ce21224f33396bc71ffa770918bf62b6f1b0
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-06 15:35:49 +0100

    Refactor DokoImage component

--- src/lib/DokoImage.svelte
+++ src/lib/DokoImage.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-    export let src = "nagi-amedoko.jpeg";
+    import nagiGator from "$lib/assets/nagi-amedoko.jpeg";
+    
+    export let src = nagiGator;
+    export let url = "https://twitter.com/Nagi_Nyaaa/status/1598539547184230400";
     export let imageName = "Ame Doko gator by Nabi";
 </script>
 
-<a
-    href="https://twitter.com/Nagi_Nyaaa/status/1598539547184230400"
-    class="mx-auto my-3 max-w-lg drop-shadow-lg"
->
-    <img {src} alt={imageName} class="object-contain" width="1000" height="1000" />
+<a href={url} class="mx-auto my-3 max-w-lg drop-shadow-lg">
+    <img
+        {src}
+        alt={imageName}
+        class="object-contain"
+        width="1000"
+        height="1000"
+    />
 </a>
