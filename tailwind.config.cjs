e3bc9d59eefb1f900253b8b6f73cbadd4d7b29ab
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-15 12:03:25 +0100

    Update styling on Watch Now button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'ame-light-yellow': '#F2D7B6',
-        'ame-dark-brown': '#40302b'
+        'ame-dark-brown': '#40302b',
+        'ame-red': '#BF545D'
       }
     },
   },
